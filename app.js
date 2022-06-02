d0c0e944d04cb5f3ef2b5cc72853bb2fcca5f90d
MioRain <ericzero1593@hotmail.com>
2022-06-02 19:28:53 +0800

    feat: add welcome page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser"
 import db from "./config/mongoose.js"
+import userAccount from "./models/user-account.js"
 
 const app = express()
 const port = 3000
@@ -15,6 +16,21 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const result = await userAccount.findOne({ email, password }).lean()
+    if (result) {
+      res.render('welcome', { result })
+    } else {
+      res.render('index', { tipMessage: '帳號或密碼錯誤' })
+    }
+  }
+  catch (err) {
+    console.err('err', err)
+  }
 })
 
 app.listen(port, () => {
